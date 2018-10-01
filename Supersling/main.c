7d6b61b014b3d915a2be4cfa1df56cf0a17dfec9
Wilson Styres <wstyres@gmail.com>
2018-10-01 10:11:29 -0400

    [Supersling] Add more patches to platformize Supersling on iOS 11

--- Supersling/main.c
+++ Supersling/main.c
@@ -4,45 +4,38 @@
 /* Set platform binary flag */
 #define FLAG_PLATFORMIZE (1 << 1)
 
-void platformize_me() {
-    void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
-    if (!handle) return;
-
-    // Reset errors
-    dlerror();
-    typedef void (*fix_entitle_prt_t)(pid_t pid, uint32_t what);
-    fix_entitle_prt_t ptr = (fix_entitle_prt_t)dlsym(handle, "jb_oneshot_entitle_now");
-
-    const char *dlsym_error = dlerror();
-    if (dlsym_error) return;
-
-    ptr(getpid(), FLAG_PLATFORMIZE);
-}
-
-void patch_setuid() {
+void patch_setuidandplatformize() {
   void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
-  if (!handle)
-  return;
+  if (!handle) return;
 
   // Reset errors
   dlerror();
+
   typedef void (*fix_setuid_prt_t)(pid_t pid);
-  fix_setuid_prt_t ptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
+  fix_setuid_prt_t setuidptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
+
+  typedef void (*fix_entitle_prt_t)(pid_t pid, uint32_t what);
+  fix_entitle_prt_t entitleptr = (fix_entitle_prt_t)dlsym(handle, "jb_oneshot_entitle_now");
+
+  setuidptr(getpid());
+
+  setuid(0);
 
   const char *dlsym_error = dlerror();
-  if (dlsym_error)
-  return;
+  if (dlsym_error) {
+    return;
+  }
 
-  ptr(getpid());
+  entitleptr(getpid(), FLAG_PLATFORMIZE);
 }
 
 int main(int argc, char ** argv) {
-  platformize_me();
-  patch_setuid();
+  patch_setuidandplatformize();
+
   setuid(0);
   setgid(0);
 
-  int result = execvp(argv[1], &argv[1]);
+  int result = execv(argv[1], &argv[1]);
 
   return result;
 }
