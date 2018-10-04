8209eb4c950f1bd790beea88482a88f90dffd750
Wilson Styres <wstyres@gmail.com>
2018-10-03 20:44:20 -0400

    [Packages] Add new methods to AUPMPackageManager header

--- AUPM/Packages/AUPMPackageManager.h
+++ AUPM/Packages/AUPMPackageManager.h
@@ -1,3 +1,7 @@
+@class AUPMPackage;
+
 @interface AUPMPackageManager : NSObject
 - (NSArray *)installedPackageList;
+- (BOOL)packageHasTweak:(AUPMPackage *)package;
+- (BOOL)packageHasApp:(AUPMPackage *)package;
 @end
