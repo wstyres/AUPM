86d626ac91d5086410909d131b466be4ded37fff
Wilson Styres <wstyres@gmail.com>
2018-10-29 10:28:30 -0400

    [Web] Send AUPM user agent

--- AUPM/AUPMWebViewController.m
+++ AUPM/AUPMWebViewController.m
@@ -2,22 +2,25 @@
 #import "AUPMRefreshViewController.h"
 #import <Realm/Realm.h>
 
-@implementation AUPMWebViewController {
+@interface AUPMWebViewController () {
   WKWebView *_webView;
   UIProgressView *_progressView;
   NSURL *_url;
 }
+@end
+
+@implementation AUPMWebViewController
 
 - (id)init {
-    self = [super init];
+  self = [super init];
 
-    return self;
+  return self;
 }
 
 - (id)initWithURL:(NSURL *)url {
   self = [super init];
   if (self) {
-      _url = url;
+    _url = url;
   }
   return self;
 }
@@ -33,10 +36,14 @@
   configuration.userContentController = controller;
 
   _webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:configuration];
-  _webView.customUserAgent = [NSString stringWithFormat:@"AUPM-%@", PACKAGE_VERSION];
+  _webView.customUserAgent = @"AUPM-1.0~beta15";
   _webView.navigationDelegate = self;
+  [_webView setBackgroundColor:[UIColor colorWithRed:0.94 green:0.94 blue:0.96 alpha:1.0]];
+  _webView.opaque = false;
+  _webView.backgroundColor = [UIColor clearColor];
   if (_url == NULL) {
-    [_webView loadFileURL:[NSURL URLWithString:@"file:///Applications/AUPM.app/html/home.html"] allowingReadAccessToURL:[NSURL URLWithString:@"file:///Applications/AUPM.app/html/"]];
+    NSURL *url = [[NSBundle mainBundle] URLForResource:@"home" withExtension:@".html"];
+    [_webView loadFileURL:url allowingReadAccessToURL:[url URLByDeletingLastPathComponent]];
   }
   else {
     [_webView loadRequest:[[NSURLRequest alloc] initWithURL:_url]];
@@ -44,72 +51,65 @@
 
   [_webView addObserver:self forKeyPath:NSStringFromSelector(@selector(estimatedProgress)) options:NSKeyValueObservingOptionNew context:NULL];
 
-  _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0.0f, self.navigationController.navigationBar.frame.size.height + [UIApplication sharedApplication].statusBarFrame.size.height, [[UIScreen mainScreen] bounds].size.width, 9)];
+  CGFloat height = [[UIApplication sharedApplication] statusBarFrame].size.height + self.navigationController.navigationBar.frame.size.height;
+  _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, height, [[UIScreen mainScreen] bounds].size.width, 9)];
   [_webView addSubview:_progressView];
 
   [self.view addSubview:_webView];
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    if ([keyPath isEqualToString:NSStringFromSelector(@selector(estimatedProgress))] && object == _webView) {
-        [_progressView setAlpha:1.0f];
-        [_progressView setProgress:_webView.estimatedProgress animated:YES];
+  if ([keyPath isEqualToString:NSStringFromSelector(@selector(estimatedProgress))] && object == _webView) {
+    [_progressView setAlpha:1.0f];
+    [_progressView setProgress:_webView.estimatedProgress animated:YES];
 
-        if(_webView.estimatedProgress >= 1.0f) {
-            [UIView animateWithDuration:0.3 delay:0.3 options:UIViewAnimationOptionCurveEaseOut animations:^{
-                [_progressView setAlpha:0.0f];
-            } completion:^(BOOL finished) {
-                [_progressView setProgress:0.0f animated:NO];
-            }];
-        }
+    if (_webView.estimatedProgress >= 1.0f) {
+      [UIView animateWithDuration:0.3 delay:0.3 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        [self->_progressView setAlpha:0.0f];
+      } completion:^(BOOL finished) {
+        [self->_progressView setProgress:0.0f animated:NO];
+      }];
     }
-    else {
-        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
-    }
-}
-
-- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id <UIViewControllerTransitionCoordinator>)coordinator {
-    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-
-    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-        _webView.frame = self.view.frame;
-    } completion:nil];
+  }
+  else {
+    [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+  }
 }
 
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
-    NSArray *contents = [message.body componentsSeparatedByString:@"~"];
-    NSString *destination = (NSString *)contents[0];
-    NSString *action = contents[1];
+  NSArray *contents = [message.body componentsSeparatedByString:@"~"];
+  NSString *destination = (NSString *)contents[0];
+  NSString *action = contents[1];
 
-    NSLog(@"[AUPM] Web message %@", contents);
+  NSLog(@"[AUPM] Web message %@", contents);
 
-    if ([destination isEqual:@"local"]) {
-      if ([action isEqual:@"nuke"]) {
-        [self nukeDatabase];
-      }
-      else if ([action isEqual:@"sendBug"]) {
-        [self sendBugReport];
-      }
+  if ([destination isEqual:@"local"]) {
+    if ([action isEqual:@"nuke"]) {
+      [self nukeDatabase];
     }
-    else if ([destination isEqual:@"web"]) {
-      AUPMWebViewController *webViewController = [[AUPMWebViewController alloc] initWithURL:[NSURL URLWithString:action]];
-      [[self navigationController] pushViewController:webViewController animated:true];
+    else if ([action isEqual:@"sendBug"]) {
+      [self sendBugReport];
     }
+  }
+  else if ([destination isEqual:@"web"]) {
+    AUPMWebViewController *_webViewController = [[AUPMWebViewController alloc] initWithURL:[NSURL URLWithString:action]];
+    [[self navigationController] pushViewController:_webViewController animated:true];
+  }
 }
 
-- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+- (void)_webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
   [self.navigationItem setTitle:[webView title]];
-
-  if (url == NULL) {
-#if TARGET_IPHONE_SIMULATOR
-        [webView evaluateJavaScript:@"document.getElementById('neo').innerHTML = 'Wake up, Neo...'" completionHandler:nil];
-#else
-        [webView evaluateJavaScript:[NSString stringWithFormat:@"document.getElementById('neo').innerHTML = \"You are running AUPM Version %@\"", @"1.0~beta15"] completionHandler:nil];
-#endif
+  if (_url == NULL) {
+    #if TARGET_IPHONE_SIMULATOR
+    [webView evaluateJavaScript:@"document.getElementById('neo').innerHTML = 'Wake up, Neo...'" completionHandler:nil];
+    #else
+    [webView evaluateJavaScript:[NSString stringWithFormat:@"document.getElementById('neo').innerHTML = \"You are running AUPM Version %@\"", @"1.0~beta15"] completionHandler:nil];
+    #endif
   }
 }
 
 - (void)nukeDatabase {
+  NSLog(@"Nuke action");
   AUPMRefreshViewController *refreshViewController = [[AUPMRefreshViewController alloc] init];
 
   [[UIApplication sharedApplication] keyWindow].rootViewController = refreshViewController;
@@ -120,7 +120,7 @@
     NSString *iosVersion = [NSString stringWithFormat:@"%@ running iOS %@", [[UIDevice currentDevice] model], [[UIDevice currentDevice] systemVersion]];
     RLMRealmConfiguration *config = [RLMRealmConfiguration defaultConfiguration];
     NSString *databaseLocation = [[config fileURL] absoluteString];
-    NSString *message = [NSString stringWithFormat:@"iOS Version: %@\nAUPM Version: %@\nAUPM Database Location: %@\n\nPlease describe the bug you are experiencing or feature you are requesting below: \n\n", iosVersion, PACKAGE_VERSION, databaseLocation];
+    NSString *message = [NSString stringWithFormat:@"iOS Version: %@\nAUPM Version: 1.0~beta15\nAUPM Database Location: %@\n\nPlease describe the bug you are experiencing or feature you are requesting below: \n\n", iosVersion, databaseLocation];
 
     MFMailComposeViewController *mail = [[MFMailComposeViewController alloc] init];
     mail.mailComposeDelegate = self;
@@ -137,7 +137,7 @@
 }
 
 - (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
-    [self dismissViewControllerAnimated:YES completion:NULL];
+  [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 @end
