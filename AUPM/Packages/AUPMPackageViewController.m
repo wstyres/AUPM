2673df5884c26679d8530163bf72b8cb785859e9
Wilson Styres <wstyres@gmail.com>
2018-10-29 10:16:31 -0400

    [Packages] Package depicictions will open links in new pages

--- AUPM/Packages/AUPMPackageViewController.m
+++ AUPM/Packages/AUPMPackageViewController.m
@@ -7,13 +7,18 @@
 #import <MobileGestalt/MobileGestalt.h>
 #include <sys/sysctl.h>
 
-@implementation AUPMPackageViewController {
+@interface AUPMPackageViewController () {
 	BOOL _isFinishedLoading;
 	WKWebView *_webView;
 	AUPMPackage *_package;
 	UIProgressView *_progressBar;
 	NSTimer *_progressTimer;
+	BOOL loadFailed;
 }
+
+@end
+
+@implementation AUPMPackageViewController
 
 - (id)initWithPackage:(AUPMPackage *)package {
 	_package = package;
@@ -34,9 +39,18 @@
 	CGFloat height = [[UIApplication sharedApplication] statusBarFrame].size.height + self.navigationController.navigationBar.frame.size.height + self.tabBarController.tabBar.frame.size.height;
 	_webView = [[WKWebView alloc] initWithFrame:CGRectMake(0,0, self.view.frame.size.width, self.view.frame.size.height - height)];
 	_webView.customUserAgent = @"Cydia/ButActuallyAUPM";
+	_webView.opaque = false;
+	_webView.backgroundColor = [UIColor clearColor];
 	[_webView setNavigationDelegate:self];
+	NSURL *depictionURL = [NSURL URLWithString: [_package depictionURL]];
 
-	[_webView loadFileURL:[NSURL URLWithString:@"file:///Applications/AUPM.app/html/package_depiction.html"] allowingReadAccessToURL:[NSURL URLWithString:@"file:///Applications/AUPM.app/html/"]];
+	if (depictionURL != NULL) {
+		[_webView loadRequest:[[NSURLRequest alloc] initWithURL:depictionURL]];
+	}
+	else {
+		NSURL *url = [[NSBundle mainBundle] URLForResource:@"package_depiction" withExtension:@".html"];
+		[_webView loadFileURL:url allowingReadAccessToURL:[url URLByDeletingLastPathComponent]];
+	}
 
 	_webView.allowsBackForwardNavigationGestures = true;
 	_progressBar = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width, 9)];
@@ -46,12 +60,26 @@
 	self.title = [_package packageName];
 }
 
-- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id <UIViewControllerTransitionCoordinator>)coordinator {
-    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+	NSURLRequest *request = [navigationAction request];
+	NSURL *url = [request URL];
 
-    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-        _webView.frame = self.view.frame;
-    } completion:nil];
+	if ([[url absoluteString] isEqualToString:[_package depictionURL]] || [_package depictionURL] == NULL || loadFailed) {
+		decisionHandler(WKNavigationActionPolicyAllow);
+	}
+	else {
+		AUPMWebViewController *webViewController = [[AUPMWebViewController alloc] initWithURL:url];
+		[[self navigationController] pushViewController:webViewController animated:true];
+		decisionHandler(WKNavigationActionPolicyCancel);
+	}
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id <UIViewControllerTransitionCoordinator>)coordinator {
+	[super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+
+	[coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+		self->_webView.frame = self.view.frame;
+	} completion:nil];
 }
 
 - (NSString *)generateDepiction {
@@ -69,9 +97,9 @@
 
 - (void)configureNavButton {
 	if ([_package isInvalidated]) {
-    NSLog(@"[AUPM] Object Invalidated!");
+		NSLog(@"[AUPM] Object Invalidated!");
 		[[self navigationController] popViewControllerAnimated:true];
-  }
+	}
 	else {
 		if ([_package isInstalled]) {
 			if ([_package isFromRepo]) {
@@ -97,7 +125,7 @@
 		[alert dismissViewControllerAnimated:true completion:nil];
 
 		AUPMQueue *queue = [AUPMQueue sharedInstance];
-		[queue addPackage:_package toQueueWithAction:AUPMQueueActionRemove];
+		[queue addPackage:self->_package toQueueWithAction:AUPMQueueActionRemove];
 
 		AUPMQueueViewController *queueVC = [[AUPMQueueViewController alloc] init];
 		UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:queueVC];
@@ -108,7 +136,7 @@
 		[alert dismissViewControllerAnimated:true completion:nil];
 
 		AUPMQueue *queue = [AUPMQueue sharedInstance];
-		[queue addPackage:_package toQueueWithAction:AUPMQueueActionReinstall];
+		[queue addPackage:self->_package toQueueWithAction:AUPMQueueActionReinstall];
 
 		AUPMQueueViewController *queueVC = [[AUPMQueueViewController alloc] init];
 		UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:queueVC];
@@ -157,16 +185,21 @@
 	_isFinishedLoading = TRUE;
 
 	NSURL *depictionURL = [NSURL URLWithString:[_package depictionURL]];
-	if (depictionURL != NULL) {
+	if (depictionURL == NULL || loadFailed) {
 		[_webView evaluateJavaScript:[NSString stringWithFormat:@"document.getElementById('package').innerHTML = '%@ (%@)';", [_package packageName], [_package packageIdentifier]] completionHandler:nil];
 		[_webView evaluateJavaScript:[NSString stringWithFormat:@"document.getElementById('version').innerHTML = 'Version %@';", [_package version]] completionHandler:nil];
-		[_webView evaluateJavaScript:[NSString stringWithFormat:@"document.getElementById('desc').innerHTML = '%@';", [_package packageDescription]] completionHandler:^(id Result, NSError * error) {
-			NSLog(@"[AUPM] Error: %@ Description: %@", error, [_package packageDescription]);
-		}];
-		NSString *command = [NSString stringWithFormat:@"document.getElementById('depiction-src').src = '%@';", [depictionURL absoluteString]];
-		[_webView evaluateJavaScript:command completionHandler:^(id Result, NSError * error) {
-			NSLog(@"[AUPM] Error: %@", error);
-		}];
+
+		NSLog(@"%@", [_package packageDescription]);
+		if ([_package packageDescription] == NULL || [[_package packageDescription] isEqual:@""])  {
+			[_webView evaluateJavaScript:@"var element = document.getElementById('desc');element.parentNode.parentNode.outerHTML = '';" completionHandler:nil];
+		}
+		else {
+			[_webView evaluateJavaScript:[NSString stringWithFormat:@"document.getElementById('desc').innerHTML = \"%@\";", [_package packageDescription]] completionHandler:nil];
+		}
+		//        NSString *command = [NSString stringWithFormat:@"document.getElementById('depiction-src').src = '%@';", [depictionURL absoluteString]];
+		//        [_webView evaluateJavaScript:command completionHandler:^(id Result, NSError * error) {
+		//            NSLog(@"[AUPM] Error: %@", error);
+		//        }];
 	}
 }
 
@@ -174,10 +207,10 @@
 	if (_isFinishedLoading) {
 		if (_progressBar.progress >= 1) {
 			[UIView animateWithDuration:0.3 delay:0.3 options:0 animations:^{
-				_progressBar.alpha = 0.0;
+				self->_progressBar.alpha = 0.0;
 			} completion:^(BOOL finished) {
-				[_progressTimer invalidate];
-				_progressTimer = nil;
+				[self->_progressTimer invalidate];
+				self->_progressTimer = nil;
 			}];
 		}
 		else {
@@ -193,7 +226,9 @@
 		}
 
 		if (_progressBar.progress == 1.0) {
-			[_webView loadHTMLString:[self generateDepiction] baseURL:nil];
+			loadFailed = true;
+			NSURL *url = [[NSBundle mainBundle] URLForResource:@"package_depiction" withExtension:@".html"];
+			[_webView loadFileURL:url allowingReadAccessToURL:[url URLByDeletingLastPathComponent]];
 		}
 	}
 }
